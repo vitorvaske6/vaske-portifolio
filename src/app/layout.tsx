2b9fb12ce8a598c530ae7984d64b5a4f26a53c78
vitorvaske6 <vitorvaske6@gmail.com>
2023-10-15 18:15:57 -0300

    20231015.0 - Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { Sidebar } from '@/components/navigation/Sidebar'
 import { Navigation } from '@/components/navigation/Navigation'
 import Head from 'next/head'
 import { Metadata } from 'next'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,15 +16,26 @@ export default function Layout({
   children,
 }: {
   children: React.ReactNode
-  }) {
+}) {
   const { currentTheme } = useStateContext();
 
   return (
     <html lang="en" className='scroll-smooth scrollbar-thin scrollbar-thumb-white scrollbar-track-gray-950'>
       <head>
         <title>Vaske - Portifolio</title>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-M09JXKHT79" />
+        <script dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-M09JXKHT79');
+          `
+        }} />
       </head>
-       <body className={`${inter.className} bg-gray-950 min-w-[484px]`}> 
+      <body className={`${inter.className} bg-gray-950 min-w-[484px]`}>
         <NextAuthProvider>
           <ContextProvider>
             <NextUIProviderFunction theme={currentTheme}>
